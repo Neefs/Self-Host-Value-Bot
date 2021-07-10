44b84f077fd74c4883125b7c2b4cd92ae8d3d9b3
EzBedWars <ezbedwars@gmail.com>
2021-07-10 01:36:31 -0400

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
+
+
+
 # Value-Bot
- 
+---
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/Neefs/Value-Bot/graphs/commit-activity)
+[![GitHub license](https://img.shields.io/github/license/Neefs/Value-Bot.svg)](https://github.com/Neefs/Value-Bot/blob/main/LICENSE)
+[![Github all releases](https://img.shields.io/github/downloads/Neefs/Value-Bot/total.svg)](https://github.com/Neefs/Value-Bot/releases/)
+[![GitHub pull-requests](https://img.shields.io/github/issues-pr/Neefs/Value-Bot.svg)](https://github.com/Neefs/Value-Bot/pull/)
+[![GitHub issues](https://img.shields.io/github/issues/Neefs/Value-Bot.svg)](https://github.com/Neefs/Value-Bot/issues/)
+
+Ever want to keep trace of the money someone is making? Whether it is a minecraft server with an economy or a made up currency this is the bot for you. This is bot and easy to setup. 
+
+---
+
+## Setup
+### **Requirements**
+    aiosqlite
+    jishaku
+    mystbin.py
+    [discord.py](https://github.com/Rapptz/discord.py)>=2.0
+
+### **Coniguration**
+Head over to default_config.json and rename it to config.json.
+
+Now lets get into it!
+```json
+    {
+    "bot": {
+        "token": "You bots Token found at https://discord.com/developers/applications/",
+        "prefix": "A prefix of your choice."
+    },
+    "value": {
+        "role": 0 //The roleid of the role you want to have access to add, remove, reset
+    }
+}
+```
+
+---
+## Discord
+
+If you want to check out this project or any other project join the discord.
+
+[Discord Invite](https://discord.gg/P9bW4Ug4kx)
+
+
+
+
+
+
+
---
+++ sqlite.py
@@ -0,0 +1,142 @@
+import aiosqlite
+import discord
+import asyncio
+
+async def insert_data(database: str=None, tables: list=None, checks: list=None) -> bool:
+    db = await aiosqlite.connect("data/database.db")
+    cur = await db.cursor()
+
+    where_val = None
+
+    if checks:
+        where_val = "WHERE "
+        for check, ans in checks:
+            where_val += f"{check} = {ans} AND "
+        where_val = where_val[:-4]
+
+    table_names = ""
+    table_values = []
+    for name, value in tables:
+        table_names += f"{name}, "
+        table_values.append(value)
+    
+    table_names = table_names[:-2]
+
+    try:
+        if where_val:
+            await db.execute(f"INSERT INTO {database} ({table_names}) VALUES ({('?, ' * len(tables))[:-2]}) {where_val}", tuple(table_values))
+        else:
+            await db.execute(f"INSERT INTO {database} ({table_names}) VALUES ({('?, ' * len(tables))[:-2]})", tuple(table_values))
+        await db.commit()
+    except Exception as e:
+        raise e
+        return False
+
+    else:
+        return True
+    finally:
+        await db.close()
+
+
+async def update_data(database: str=None, tables: list=None, checks: list=None) -> bool:
+    db = await aiosqlite.connect("data/database.db")
+    cur = await db.cursor()
+
+    where_val = None
+
+    if checks:
+        where_val = "WHERE "
+        for check, ans in checks:
+            where_val += f"{check} = {ans} AND "
+        where_val = where_val[:-4]
+
+    table_names = ""
+    table_values = []
+    for name, value in tables:
+        table_names += f"{name} = ?,"
+        table_values.append(value)
+    
+    table_names = table_names[:-1]
+
+    try:
+        if where_val:
+            await db.execute(f"UPDATE {database} SET {table_names} {where_val}", tuple(table_values))
+        else:
+            await db.execute(f"UPDATE {database} SET {table_names}", tuple(table_values))
+        await db.commit()
+    except Exception as e:
+        return False
+
+    else:
+        return True
+    finally:
+        await db.close()
+
+
+async def read_data(database: str=None, tables: list=None, checks: list=None, order: list=None) -> tuple:
+    db = await aiosqlite.connect("data/database.db")
+    cur = await db.cursor()
+
+    where_val = ""
+    order_val = ""
+
+    if checks:
+        where_val = "WHERE "
+        where_ans = []
+        for check, ans in checks:
+            where_val += f"{check} = ? AND "
+            where_ans.append(ans)
+        where_val = where_val[:-4]
+
+    if order:
+        order_val = "ORDER BY "
+        for val in order:
+            order_val += f"{val}, "
+        order_val = order_val[:-2]
+        
+
+    table_names = ""
+    for name in tables:
+        table_names += f"{name},"
+    
+    table_names = table_names[:-1]
+
+    try:
+        if where_val:
+            await cur.execute(f"SELECT {table_names} FROM {database} {where_val} {order_val}", tuple(where_ans))
+            output = await cur.fetchall()
+        else:
+            await cur.execute(f"SELECT {table_names} FROM {database} {order_val}")
+            output = await cur.fetchall()
+
+    except Exception as e:
+        pass
+    else:
+        return output
+    finally:
+        await db.close()
+
+async def remove_data(database: str=None, checks: list=None) -> None:
+    db = await aiosqlite.connect("data/database.db")
+    cur = await db.cursor()
+
+    where_val = None
+
+    if checks:
+        where_val = "WHERE "
+        where_ans = []
+        for check, ans in checks:
+            where_val += f"{check} = ? AND "
+            where_ans.append(ans)
+        where_val = where_val[:-4]
+
+        try:
+            await cur.execute(f"DELETE FROM {database} {where_val}", tuple(where_ans))
+            await db.commit()
+        
+        except Exception as e:
+            pass
+        else:
+            pass
+        finally:
+            await db.close()
